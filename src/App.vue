da178c90190af93cd1b2e9964c33fecfe9124403
sardar14 <sardar@uod.ac>
2022-09-26 13:38:29 +0300

    'test'

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+<div class="container-fluid">
+     <nav class="navbar navbar-expand-sm navbar-light bg-light shadow-sm">
+         <div class="container">
+         <a class="navbar-brand" href="#">Navbar</a>
+         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+           aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="collapsibleNavId">
+           <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+             <li class="nav-item">
+                  <router-link to="/" class="nav-link active" href="#" aria-current="page">Home</router-link> 
+                  <span class="visually-hidden">(current)</span>
+             </li>
+             <li class="nav-item">
+                 <router-link class="nav-link" to="/about">About</router-link> 
+             </li>
+              <li class="nav-item">
+                 <router-link class="nav-link" to="/test">test1</router-link> 
+             </li>
+           </ul>
+  
+         </div>
+       </div>
+     </nav>
+
+<div class="container">
+     <RouterView />
+</div>
+</div>
+
+</template>
+
+<style>
+body{
+  background-color:#dfd8d8;
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,79 @@
+<template>
+
+  <h1>Number of item {{notes.length}}</h1>
+  <div class="list-group">
+    <li class="list-group-item d-flex align-items-cente col-sm-4 shadow-sm justify-content-between align-items-center"  
+    v-for="note in notes" :key="note.text"
+    >
+        <span> {{note.text}}</span>
+        <span class="badge badge-pill">
+            <button class="btn btn-danger"  @click="removeCar(note)">X</button>
+        </span>
+        <span class="badge badge-pill">
+           <div>
+                <input type="checkbox" :checked="note.isDone"/> 
+           </div>
+        </span>
+    </li>
+   </div>
+   <div class="mb-3 col-sm-4">
+    <label for="" class="form-label">Add Car</label>
+    <input type="text" v-model="item" @keypress.enter ="addItem" 
+    class="form-control shadow-sm" name="" id="" aria-describedby="helpId" placeholder="Add car">
+    <small id="helpId" class="form-text text-muted"></small>
+</div>
+<button type="submit" class="btn btn-primary" @click="addItem">Add item #{{this.notes.length +1}}</button>
+
+
+</template>
+
+<script>
+export default {
+
+name:'my-first',
+data()
+{
+    return{
+        title:'Azad', 
+        item:'',
+        notes:[
+            {text:'Item1', isDone:true},
+            {text:'Item2', isDone:false},
+            {text:'Item3', isDone:false},
+            {text:'Item4', isDone:true},
+        ]
+    }
+}, 
+methods:{
+    addItem()
+    {
+        if(this.item.length!==0)
+        {
+            this.notes.push({
+            text : this.item,
+            isDone :false
+         });
+         this.item =''
+
+        }
+    }, 
+    removeCar(val)
+    {
+        this.notes = this.notes.filter(function(item) {
+               return item.text !== val.text
+        })
+    },
+}, 
+computed:{
+        itemsInList (){
+              return this.notes.length
+        }
+       
+    }
+
+}
+</script>
+
+<style>
+
+</style>
